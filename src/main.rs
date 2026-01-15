b7bcbe31795639e789292cc73672b7fcb6c73d96
Denis Komarov <dennkom1@mail.ru>
2026-01-15 13:53:13 +0300

    FIX token

--- src/main.rs
+++ src/main.rs
@@ -43,7 +43,7 @@ async fn main() {
 
     //Либа для подтяжки данных из файла .env
     dotenvy::dotenv().ok();
-    let bot_token = env::var("TG_TOKEN_TEST").expect("Токен бота не найден");
+    let bot_token = env::var("TG_TOKEN").expect("Токен бота не найден");
     let ai_token = env::var("API_TOKEN").expect("Токен AI не найден");
     info!("Токен AI модели загружен");
     let bot = Bot::new(bot_token);
